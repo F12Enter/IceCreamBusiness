42b6a12d691b4d13d549e40b41d45445eeb8968f
AkhmedKarim <i.love.you.kyiv@gmail.com>
2025-04-05 02:09:48 +0300

    first

--- Assets/__Scripts/Core/Statistics/EndDayInfo.cs
+++ Assets/__Scripts/Core/Statistics/EndDayInfo.cs
@@ -2,33 +2,107 @@ using UnityEngine;
 using Core.IceCreamMaker;
 using Core.OrderSystem;
 using TMPro;
+using System.Collections.Generic;
+using System.Linq;
+using Player.Controller;
 
 namespace Core.Statistics
 {
     public class EndDayInfo : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _wastedToday;
-        [SerializeField] private TextMeshProUGUI _moneyBalance;
-        [SerializeField] private TextMeshProUGUI _moneyGeneral;
+        [Header("Global Information")]
+        [SerializeField] private TextMeshProUGUI _totalIcecreamBallsInfo;
+        [SerializeField] private TextMeshProUGUI _currentBalanceInfo;
 
-        [SerializeField] private TextMeshProUGUI _vanillaLasts;
-        [SerializeField] private TextMeshProUGUI _chocolateLasts;
-        [SerializeField] private TextMeshProUGUI _strawberryLasts;
+        [Header("Daily Information")]
+        [SerializeField] private TextMeshProUGUI _spentScoopsPerDayInfo;
+        [SerializeField] private TextMeshProUGUI _dailyProfitInfo;
+
+
+        [Header("Shop Panel")]
+        [SerializeField] private TMP_InputField _vanillaBallsInputField;
+        [SerializeField] private TMP_InputField _chocolateBallsInputField;
+        [SerializeField] private TMP_InputField _strawberryBallsInputField;
+        [SerializeField] private TextMeshProUGUI _totalCostInfo;
+
+        List<TMP_InputField> _shopsInputFieldsList;
+
+        private void Awake()
+        {
+            _shopsInputFieldsList = new List<TMP_InputField>
+            {
+                _vanillaBallsInputField,
+                _chocolateBallsInputField,
+                _strawberryBallsInputField,
+            };
+        }
+       
 
         private void OnEnable()
         {
+            //CursorManager.UnlockCursor();я так розумію він вже десь лочиться та анлочітся
+
+            foreach (var inputField in _shopsInputFieldsList)
+            {
+                inputField.onValueChanged.AddListener(OnSomeShopsInputFieldChang);
+            }
+
             FillInfo();
+        }
+        private void OnDisable()
+        {
+            //CursorManager.LockCursor();
+
+            foreach (var inputField in _shopsInputFieldsList)
+            {
+                inputField.onValueChanged.RemoveListener(OnSomeShopsInputFieldChang);
+            }
+        }
+        private void Update()
+        {
+            CursorManager.UnlockCursor();//для тесту
         }
 
         public void FillInfo()
         {
-            _wastedToday.text = IceCreamMachine.Instance.WastedBalls.ToString();
-            _moneyBalance.text = CustomerManager.Instance.MoneyPerDay.ToString();
-            _moneyGeneral.text = Economy.EconomyManager.Money.ToString();
+            int spentScoopsPerDayCount =
+               IceCreamStorage.VanillaCount +
+               IceCreamStorage.ChocolateCount +
+               IceCreamStorage.StrawberryCount;
 
-            _vanillaLasts.text = IceCreamStorage.VanillaCount.ToString();
-            _chocolateLasts.text = IceCreamStorage.ChocolateCount.ToString();
-            _strawberryLasts.text = IceCreamStorage.StrawberryCount.ToString();
+            //Global Information
+            _totalIcecreamBallsInfo.text = IceCreamMachine.Instance.WastedBalls.ToString();
+            _currentBalanceInfo.text = Economy.EconomyManager.Money.ToString();
+
+            //Daily Information
+            _spentScoopsPerDayInfo.text = spentScoopsPerDayCount.ToString();
+            _dailyProfitInfo.text = Economy.EconomyManager.Money.ToString();//тут повинен бути прибуток за день
+
+            //Shop
+            SetTotalCost();
+            //_vanillaBallsInputField.text = IceCreamStorage.VanillaCount.ToString();
+            //_chocolateBallsInputField.text = IceCreamStorage.ChocolateCount.ToString();
+            //_strawberryBallsInputField.text = IceCreamStorage.StrawberryCount.ToString();
+        }
+
+
+
+        void OnSomeShopsInputFieldChang(string val)
+        {
+            SetTotalCost();
+        }
+        void SetTotalCost()
+        {
+            int totalCost = _shopsInputFieldsList
+                .Select(field => int.TryParse(field.text, out int val) ? val : 0)
+                .Sum();
+
+            _totalCostInfo.text = totalCost.ToString();
+        }
+
+        public void OnBuyButtonClick()
+        {
+            Debug.Log("Buy!");
         }
 
     }
